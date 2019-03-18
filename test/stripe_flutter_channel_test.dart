b4bed0d7e6fcaab3766843c926bfdd359678f80a
Jack <jack.liu@me.hk>
2019-03-18 11:10:17 +0800

    update remove test code

--- test/stripe_flutter_channel_test.dart
+++ test/stripe_flutter_channel_test.dart
@@ -1,18 +1,3 @@
 void main() {
-  //TODO test
-//  const MethodChannel channel = MethodChannel('stripe_flutter_channel');
-//
-//  setUp(() {
-//    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-//      return '42';
-//    });
-//  });
-//
-//  tearDown(() {
-//    channel.setMockMethodCallHandler(null);
-//  });
-//
-//  test('getPlatformVersion', () async {
-//    expect(await StripeFlutterChannel.platformVersion, '42');
-//  });
+  //TODO: package test
 }
